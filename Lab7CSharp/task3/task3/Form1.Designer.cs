c2f4eb4f623c0ed356fead35107283b7b0496283
OlexanderVanzuriak <vanzuriak.oleksandr@chnu.edu.ua>
2024-03-28 10:41:39 +0200

    task3 done

---
+++ Lab7CSharp/task3/task3/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(25, 259);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Draw";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(318, 64);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(324, 259);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(147, 259);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 2;
+            button2.Text = "Clear";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Трикутник";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(126, 64);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "Green";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Прямокутник";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(126, 110);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 6;
+            textBox2.Text = "Red";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(74, 151);
+            label3.Name = "label3";
+            label3.Size = new Size(45, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Еліпс";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(126, 151);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 8;
+            textBox3.Text = "Blue";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(69, 188);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 20);
+            label4.TabIndex = 9;
+            label4.Text = "Ромб";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(126, 188);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 10;
+            textBox4.Text = "Purple";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Button button2;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+    }
+}
---
+++ Lab7CSharp/task3/task3/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace task3
+{
+    public partial class Form1 : Form
+    {
+        private List<Shape> shapes = new List<Shape>();
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public enum ShapeType
+        {
+            Rectangle,
+            Triangle,
+            Circle,
+            Rhombus
+        }
+
+        public abstract class Shape
+        {
+            protected int x;
+            protected int y;
+
+            public Shape(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public abstract void Draw(Graphics g);
+            public void MoveTo(int newX, int newY)
+            {
+                x = newX;
+                y = newY;
+            }
+        }
+
+        // Похідний клас для трикутника
+        // Похідний клас для прямокутника
+        public class RectangleShape : Shape
+        {
+            private readonly int width;
+            private readonly int height;
+
+            public RectangleShape(int x, int y, int width, int height) : base(x, y)
+            {
+                this.width = width;
+                this.height = height;
+            }
+
+            public override void Draw(Graphics g)
+            {
+                g.DrawRectangle(Pens.Green, new Rectangle(x, y, width, height));
+            }
+        }
+
+        // Похідний клас для трикутника
+        public class TriangleShape : Shape
+        {
+            private readonly int sideLength;
+
+            public TriangleShape(int x, int y, int sideLength) : base(x, y)
+            {
+                this.sideLength = sideLength;
+            }
+
+            public override void Draw(Graphics g)
+            {
+                Point[] points =
+                {
+            new Point(x + sideLength / 2, y),
+            new Point(x, y + sideLength),
+            new Point(x + sideLength, y + sideLength)
+        };
+                g.DrawPolygon(Pens.Red, points);
+            }
+        }
+
+        // Похідний клас для кола
+        public class CircleShape : Shape
+        {
+            private readonly int radius;
+
+            public CircleShape(int x, int y, int radius) : base(x, y)
+            {
+                this.radius = radius;
+            }
+
+            public override void Draw(Graphics g)
+            {
+                g.DrawEllipse(Pens.Blue, new Rectangle(x - radius, y - radius, 2 * radius, 2 * radius));
+            }
+        }
+
+        public class RhombusShape : Shape
+        {
+            private readonly int sideLength;
+
+            public RhombusShape(int x, int y, int sideLength) : base(x, y)
+            {
+                this.sideLength = sideLength;
+            }
+
+            public override void Draw(Graphics g)
+            {
+                Point[] points =
+                {
+            new Point(x, y + sideLength / 2),
+            new Point(x + sideLength / 2, y),
+            new Point(x + sideLength, y + sideLength / 2),
+            new Point(x + sideLength / 2, y + sideLength)
+        };
+                g.DrawPolygon(Pens.Purple, points);
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            random = new Random();
+            ShapeType shapeType = (ShapeType)random.Next(4); // Змінено на 4 для врахування ромба
+
+            int x = random.Next(pictureBox1.Width);
+            int y = random.Next(pictureBox1.Height);
+
+            // Вибираємо випадкові параметри для фігури
+            int param1 = random.Next(10, 100);
+
+            // Створюємо екземпляр фігури
+            Shape shape;
+            switch (shapeType)
+            {
+                case ShapeType.Rectangle:
+                    shape = new RectangleShape(x, y, param1, param1); // Параметри змінені на param1 для квадрата
+                    break;
+                case ShapeType.Triangle:
+                    shape = new TriangleShape(x, y, param1);
+                    break;
+                case ShapeType.Circle:
+                    shape = new CircleShape(x, y, param1);
+                    break;
+                case ShapeType.Rhombus: // Додано обробку ромба
+                    shape = new RhombusShape(x, y, param1);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            shapes.Add(shape);
+
+            // Оновлюємо відображення малюнка
+            RefreshDrawing();
+        }
+
+        private void RefreshDrawing()
+        {
+            // Створюємо нове зображення
+            Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            // Рисуємо всі фігури на зображенні
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                foreach (var shape in shapes)
+                {
+                    shape.Draw(g);
+                }
+            }
+
+            // Відображаємо зображення на PictureBox
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                g.Clear(Color.White);
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                g.Clear(Color.White);
+            }
+        }
+    }
+}
